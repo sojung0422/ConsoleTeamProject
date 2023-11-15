8e4787b5c11d85eb974cdb71ed102ff800479361
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-15 15:49:32 +0900

    Creature, Character

--- TeamProject/TeamProject/Character.cs
+++ TeamProject/TeamProject/Character.cs
@@ -1,27 +1,27 @@
 ﻿namespace TeamProject
 {
-    public class Character
-    {
-        public string Name { get; }
-        public string Job { get; }
-        public int Level { get; }
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Gold { get; }
+    //public class Character
+    //{
+    //    public string Name { get; }
+    //    public string Job { get; }
+    //    public int Level { get; }
+    //    public int Atk { get; }
+    //    public int Def { get; }
+    //    public int Hp { get; }
+    //    public int Gold { get; }
 
 
-        public Character(string name, string job,
-                         int level, int atk,
-                         int def, int hp, int gold)
-        {
-            Name = name;
-            Job = job;
-            Level = level;
-            Atk = atk;
-            Def = def;
-            Hp = hp;
-            Gold = gold;
-        }
-    }
+    //    public Character(string name, string job,
+    //                     int level, int atk,
+    //                     int def, int hp, int gold)
+    //    {
+    //        Name = name;
+    //        Job = job;
+    //        Level = level;
+    //        Atk = atk;
+    //        Def = def;
+    //        Hp = hp;
+    //        Gold = gold;
+    //    }
+    //}
 }
---
+++ TeamProject/TeamProject/Creature/Creature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject {
+    public class Creature {
+        #region Properties
+        // 기본 정보: 게임 내내 변하지 않는 정보입니다.
+        public string Name { get; protected set; }
+        public float DefaultHpMax { get; protected set; }
+        public float DefaultDamage { get; protected set; }
+        public float DefaultDefense { get; protected set; }
+        public float DefaultMpMax { get; protected set; }
+        // 현재 정보: 게임 중 변할 수 있는 정보입니다.
+        public int Level { get; protected set; }
+        public virtual float Hp {
+            get => hp;
+            set {
+                if (value <= 0) hp = 0;
+                else if (value >= DefaultHpMax) hp = DefaultHpMax;
+                else hp = value;
+            }
+        }
+        #endregion
+
+        protected float hp;
+    }
+}
