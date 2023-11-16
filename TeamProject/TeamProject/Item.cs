b10cc56429dc7c7ce3446f6a3823e77b5e19acda
jyWooooo <wjy970423@gmail.com>
2023-11-16 17:23:35 +0900

    Add duplicate item processing logic to inventory
    인벤토리에 중복된 아이템을 add 했을 때의 처리 로직 추가
    추가로 Item.cs의 OnAdded 이벤트의 매개변수에 Item을 하나 더 추가해서 stackable item의 stack count를 합치기 위해 중복된 아이템의 인스턴스를 가져올 수 있게합니다.

--- TeamProject/TeamProject/Item.cs
+++ TeamProject/TeamProject/Item.cs
@@ -17,14 +17,14 @@ namespace TeamProject
 
         //아이템이 인벤토리에 추가될 때 호출될 이벤트
         //소모품의 경우 인벤토리에 같은 아이템이 있다면 새로운 칸에 들어가는게 아니라, 그 칸의 stack cnt를 +1 해야합니다.
-        event Action<Character>? OnAdded;
+        event Action<Character, Item>? OnAdded;
 
         //아이템이 인벤토리에서 삭제될 떄 호출될 이벤트
         //만약 장착중인 장비아이템이 삭제된다면, 장착해제도 같이 진행해야합니다.
         event Action<Character>? OnRemoved;
 
         public void Use(Character owner) => OnUsed?.Invoke(owner);
-        public void OnAdd(Character owner) => OnAdded?.Invoke(owner);
+        public void OnAdd(Character owner, Item duplicatedItem = null) => OnAdded?.Invoke(owner, duplicatedItem);
         public void OnRemove(Character owner) => OnRemoved?.Invoke(owner);
     }
 }
