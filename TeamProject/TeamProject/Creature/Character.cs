c46ee4f7535cfb8d86a079fac95913718f62ebff
oteosuk <oteosuk@naver.com>
2023-11-16 17:39:39 +0900

    Add Critical, Avoid
    Character.cs Creature.cs Monster.cs에 변수, 매개변수, Attack함수 추가 및 수정했습니다.

--- TeamProject/TeamProject/Creature/Character.cs
+++ TeamProject/TeamProject/Creature/Character.cs
@@ -11,6 +11,9 @@ namespace TeamProject {
         public float Damage => DefaultDamage + damageModifier;
         public float Defense => DefaultDefense + defenseModifier;
         public float MpMax => DefaultMpMax + mpMaxModifier;
+        public float Critical => DefaultCritical + criticalModifier; // 오태
+        public float Avoid => DefaultCritical + avoidModifier; // 오태
+
         public int Gold { get; protected set; }
         public override float Hp {
             get => hp;
@@ -21,29 +24,59 @@ namespace TeamProject {
             }
         }
 
-        public Character(string name, string job, int level, int damage, int defense, int hp, int gold) {
+        public Character(string name, string job, int level, int damage, int defense, int hp, int gold, float critical, float avoid) // 오태
+        {
             Name = name;
             Job = job;
             Level = level;
             DefaultDamage = damage;
             DefaultDefense = defense;
             DefaultHpMax = hp;
+            DefaultCritical = critical;
+            DefaultAvoid = avoid;
             Gold = gold;
 
             Hp = HpMax;
         }
-        public override void Attack(Creature creature)
+        public override void Attack(Creature creature) // 오태
         {
             Console.WriteLine($"{Name}이 {creature.Name}을 공격");
-            creature.OnDamaged(DefaultDamage);
+
+            if (RandomChance(creature.DefaultAvoid)) // 상대방이 회피 했을때
+            {
+                Console.WriteLine($"{creature.Name}가 회피했습니다!");
+            }
+            else // 공격에 성공 했을 때
+            {
+                // 일정 확률로 치명타 적용
+                if (RandomChance(DefaultCritical))
+                {
+                    float criticalDamage = DefaultDamage * 2; // 기본 데미지의 2배
+                    Console.WriteLine("치명타 발생!");
+                    creature.OnDamaged(criticalDamage);
+                }
+                else
+                {
+                    creature.OnDamaged(DefaultDamage); // 기본 공격
+                }
+            }
         }
 
         public override void OnDamaged(float damage)
         {
-            int finalDamage = Math.Clamp((int)damage - (int)DefaultDefense/2, 0, (int)DefaultDefense);
+            int finalDamage = Math.Clamp((int)damage - (int)DefaultDefense / 2, 0, (int)DefaultDefense);
             Console.WriteLine($"{Name}이 {finalDamage}의 데미지를 입음");
             Hp -= finalDamage;
         }
+
+        private bool RandomChance(float probability) // 오태
+        {
+            Random random = new Random();
+            float randomValue = (float)random.NextDouble(); // 0.0에서 1.0 사이의 난수
+
+            return randomValue < probability;
+        }
+
         public override bool IsDead()
         {
             if (hp <= 0) return true;
@@ -54,5 +87,7 @@ namespace TeamProject {
         private float damageModifier;
         private float defenseModifier;
         private float mpMaxModifier;
+        private float criticalModifier; // 오태
+        private float avoidModifier; // 오태
     }
 }
--- TeamProject/TeamProject/Creature/Monster.cs
+++ TeamProject/TeamProject/Creature/Monster.cs
@@ -18,20 +18,40 @@ namespace TeamProject {
                 else hp = value;
             }
         }
-        public Monster(string name, float hp, float damage, float defense, float mp)
+        public Monster(string name, float hp, float damage, float defense, float mp, float critical, float avoid) // 오태
         {
             Name = name;
             Hp = hp;
             DefaultHpMax = hp;
             DefaultDamage = damage;
             DefaultDefense = defense;
+            DefaultCritical = critical; // 오태
+            DefaultAvoid = avoid; // 오태
             DefaultMpMax = mp;
         }
 
-        public override void Attack(Creature creature)
+        public override void Attack(Creature creature) // 오태
         {
             Console.WriteLine($"{Name}이 {creature.Name}을 공격");
-            creature.OnDamaged(DefaultDamage);
+
+            if (RandomChance(creature.DefaultAvoid)) // 상대방이 회피 했을때
+            {
+                Console.WriteLine($"{creature.Name}가 회피했습니다!");
+            }
+            else // 공격에 성공 했을 때
+            {
+                // 일정 확률로 치명타 적용
+                if (RandomChance(DefaultCritical))
+                {
+                    float criticalDamage = DefaultDamage * 2; // 기본 데미지의 2배
+                    Console.WriteLine("치명타 발생!");
+                    creature.OnDamaged(criticalDamage);
+                }
+                else
+                {
+                    creature.OnDamaged(DefaultDamage); // 기본 공격
+                }
+            }
         }
 
         public override void OnDamaged(float damage)
@@ -40,6 +60,15 @@ namespace TeamProject {
             Console.WriteLine($"{Name}이 {finalDamage}의 데미지를 입음");
             Hp -= finalDamage;
         }
+
+        private bool RandomChance(float probability) // 오태
+        {
+            Random random = new Random();
+            float randomValue = (float)random.NextDouble(); // 0.0에서 1.0 사이의 난수
+
+            return randomValue < probability;
+        }
+
         public override bool IsDead()
         {
             if (hp <= 0) return true;
