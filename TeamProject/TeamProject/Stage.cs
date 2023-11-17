62bf2bb06073239af602d3e917dd24ec9bb24311
oteosuk <oteosuk@naver.com>
2023-11-17 22:11:26 +0900

    Fix Monster
    같은 몬스터가 여러마리 있을때 하나가 피해입을때 모두 영향을 받는 현상을 수정.

--- TeamProject/TeamProject/Stage.cs
+++ TeamProject/TeamProject/Stage.cs
@@ -19,24 +19,22 @@ namespace TeamProject
             MonsterMinCount = 1;
             MonsterMaxCount = 4;
         }
-        public Monster GetRandomMonster() // 오태
-        {
-            Random random = new Random();
-            return Game.Monsters[random.Next(0, Game.Monsters.Length)];
-        }
 
-        public List<Creature> MonsterSpawn()
+        public List<Creature> MonsterSpawn() // 오태 수정
         {
             MonsterMinCount += StageLevel / 10;
             MonsterMaxCount += StageLevel / 10;
             Random random = new Random();
             MonsterCount = random.Next(MonsterMinCount, MonsterMaxCount);
+
             // 스테이지별 몬스터 소환
             List<Creature> Monsters = new List<Creature>();
+
             // 몬스터 생성
             for(int i = 0; i < MonsterCount; i++)
             {
-                Creature monster = GetRandomMonster();
+                Monster randomMonster = Game.Monsters[random.Next(0, Game.Monsters.Length)];
+                Creature monster = new Monster(randomMonster); // 새로운 몬스터 인스터스 생성
                 Monsters.Add(monster);
                 //Creature monster;
                 /*switch (random.Next(0, 3))
