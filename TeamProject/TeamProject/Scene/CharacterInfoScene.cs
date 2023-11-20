b9f728fd7f74470621c09c276975a7a9f8ecfa0f
GoHyunkyu <gusrb0296@naver.com>
2023-11-20 13:58:05 +0900

    ReFactor CharacterSelectScene
    직업 선택시 어느 스탯을 가지고 있는지 확인할 수 있습니다.

--- TeamProject/TeamProject/Scene/CharacterInfoScene.cs
+++ TeamProject/TeamProject/Scene/CharacterInfoScene.cs
@@ -27,13 +27,15 @@ public class CharacterInfoScene : Scene
         Renderer.Print(3, "캐릭터의 정보가 표시됩니다.");
 
         // ==== 캐릭터 정보 표시 ====
+
+
         Renderer.Print(5, $"Lv. {Game.Player.Level}");
         Renderer.Print(6, $"{Game.Player.Name} ( {Game.Player.Job} )");
         Renderer.Print(7, $"공격력 : {Game.Player.DefaultDamage}");
         Renderer.Print(8, $"방어력 : {Game.Player.DefaultDefense}");
         Renderer.Print(9, $"체 력 : {Game.Player.Hp} / {Game.Player.DefaultHpMax}");
-        Renderer.Print(10, $"크리티컬 확률 :{Game.Player.Critical * 100: 00}%");
-        Renderer.Print(11, $"회피율 :{Game.Player.Avoid*100 : 00}%");
+        Renderer.Print(10, $"크리티컬 확률 :{Game.Player.Critical * 100:00}%");
+        Renderer.Print(11, $"회피율 :{Game.Player.Avoid * 100:00}%");
         Renderer.Print(12, $"Gold : {Game.Player.Gold} G");
 
         Renderer.PrintOptions(14, Options, true);
