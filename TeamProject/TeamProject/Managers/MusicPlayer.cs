9577591db47e00164d3e356debb1f8ed016e22c0
oteosuk <oteosuk@naver.com>
2023-11-21 23:47:11 +0900

    오디오 세팅

---
+++ TeamProject/TeamProject/Managers/MusicPlayer.cs
@@ -0,0 +1,45 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MusicPlayer
+{
+    private WaveOutEvent? waveOutEvent;
+    private AudioFileReader? audioFileReader;
+
+    public async Task PlayAsync(string filePath)
+    {
+        Stop(); // 이미 플레이 중인 경우 중지
+
+        audioFileReader = new AudioFileReader(filePath);
+        waveOutEvent = new WaveOutEvent();
+        waveOutEvent.Init(audioFileReader);
+        waveOutEvent.Play();
+
+        // 비동기로 음악이 끝날 때까지 기다림
+        await Task.Run(() =>
+        {
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(500);
+            }
+        });
+    }
+
+    public void Stop()
+    {
+        if (waveOutEvent != null)
+        {
+            waveOutEvent.Stop();
+            waveOutEvent.Dispose();
+            waveOutEvent = null;
+        }
+
+        if (audioFileReader != null)
+        {
+            audioFileReader.Dispose();
+            audioFileReader = null;
+        }
+    }
+}
--- TeamProject/TeamProject/Scene/BattleScene.cs
+++ TeamProject/TeamProject/Scene/BattleScene.cs
@@ -5,11 +5,13 @@ using System.Numerics;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace TeamProject;
 
 public class BattleScene : Scene
 {
+    private MusicPlayer musicPlayer;
     public override string Title { get; protected set; } = $"지하 감옥";
     public List<Creature> Monsters;
     public int MonsterCount;
@@ -29,8 +31,10 @@ public class BattleScene : Scene
         AttackTextList = new List<string>();
 
         OnCreatureDead += BattleEnd;
+
+        musicPlayer = new MusicPlayer();
     }
-    public override void EnterScene()
+    public override async void EnterScene()
     {
         // 몬스터 생성
         Monsters = Game.Stage.MonsterSpawn();
@@ -45,11 +49,14 @@ public class BattleScene : Scene
         AttackTextList.Add("1. 기본 공격");
         AttackTextList.Add("2. 스킬[미구현]");
 
+        musicPlayer.PlayAsync("C:\\Users\\user.JY-20191120CDZA\\Desktop\\TeamProject\\TeamProject\\TeamProject\\Sounds\\BGM1.mp3");
+
         DrawScene();
     }
 
     public override void NextScene()
     {
+        musicPlayer.Stop();
         Renderer.PrintKeyGuide(new string(' ', Console.WindowWidth - 2));
         ClearBuffer();
         for (int count = 9; count > 0; count--)
@@ -405,6 +412,8 @@ public class BattleScene : Scene
 
     public void BattleEnd(Creature creature)
     {
+        musicPlayer.Stop();
+        //await Task.Delay(5000);
         // TODO: 전투 종료
         if (creature is Monster)
         {
